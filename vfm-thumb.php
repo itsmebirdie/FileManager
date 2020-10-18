2633e349e98bba7c69e93173be0ae209044e2a52
Maanas Nair <63333626+MaanasNair@users.noreply.github.com>
2020-10-18 17:11:49 +0530

    Update thumbs

--- vfm-thumb.php
+++ vfm-thumb.php
@@ -1,16 +1,5 @@
 <?php
-/**
- * VFM - veno file manager thumb
- *
- * PHP version >= 5.3
- *
- * @category  PHP
- * @package   VenoFileManager
- * @author    Nicola Franchini <info@veno.it>
- * @copyright 2013 Nicola Franchini
- * @license   Exclusively sold on CodeCanyon: http://codecanyon.net/item/veno-file-manager-host-and-share-files/6114247
- * @link      http://filemanager.veno.it/
- */
+
 require_once 'vfm-admin/config.php';
 if ($_CONFIG['debug_mode'] === true) {
     error_reporting(E_ALL);
